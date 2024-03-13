dbcdc61119ab5d066dc6afeedc63305587d72165
Taran66 <taranpreet1911@gmail.com>
2024-03-13 22:04:25 +0530

    first commit

---
+++ src/section/index.js
@@ -0,0 +1,20 @@
+import CustomerReviews from './CustomerReviews'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import Hero from './Hero'
+
+export {
+    CustomerReviews,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer,
+    Hero
+}
+
